23c451aa7ac18526e1db08b1db35f4898177bbb7
thomaserdmenger <thomas.erdmenger@googlemail.com>
2024-02-09 10:39:02 +0100

    Style rendered content

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,15 +5,15 @@ const section = document.querySelector('.grid-container');
 
 // Render Data into Grid
 const renderContent = (arr) => {
-  // Get the conten of the array and put it into a template literal
+  // Get content of array and put it into a template literal
   const content = arr
     .map((movie) => {
-      // Get the genres in the Sub Array an put it into a templete literal
+      // Get genres in Sub Array an put it into a templete literal
       const genres = movie[4].map((genre) => `<p>${genre}</p>`).join(' ');
       return `<article>
         <h3>${movie[0]}</h3>
         <p>${movie[1]}</p>
-        <p>${movie[2]}</p>
+        <p class="bold">${movie[2]}</p>
         <p>${movie[3]}</p>
         ${genres}
         <p>${movie[5]}</p>
