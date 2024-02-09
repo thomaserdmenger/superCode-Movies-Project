8d5bd860d21dd4b11caa96ac4f13b684160814e6
thomaserdmenger <thomas.erdmenger@googlemail.com>
2024-02-09 11:21:52 +0100

    Sort content by year in descending order

--- assets/js/app.js
+++ assets/js/app.js
@@ -53,3 +53,30 @@ const sortYearUp = () => {
 };
 
 yearUpBtn.addEventListener('click', sortYearUp);
+
+// ! Absteigende Sortierung - Year Down
+
+const sortYearDown = () => {
+  const sortedYearUpContent = movies.sort((a, b) => {
+    return b[1] - a[1];
+  });
+
+  const content = sortedYearUpContent
+    .map((movie) => {
+      // Get genres in Sub Array an put it into a templete literal
+      const genres = movie[4].map((genre) => `<p>${genre}</p>`).join(' ');
+      return `<article>
+            <h3>${movie[0]}</h3>
+            <p>${movie[1]}</p>
+            <p class="bold">${movie[2]}</p>
+            <p>${movie[3]}</p>
+            ${genres}
+            <p>${movie[5]}</p>
+         </article>`;
+    })
+    .join(' ');
+
+  section.innerHTML = content;
+};
+
+yearDownBtn.addEventListener('click', sortYearDown);
